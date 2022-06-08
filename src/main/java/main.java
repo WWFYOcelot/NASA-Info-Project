03a5d61421ff2476163c7570bb784c25b45a5fc1
WWFYOcelot <48865049+WWFYOcelot@users.noreply.github.com>
2022-06-08 11:55:45 -0400

    dewit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,13 +7,14 @@ import org.json.*;
 import com.google.gson.Gson;
 
 public class main {
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
     // https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select
         Planets planetDataA = getData("https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name+from+ps&format=json");
-        }
+        start(planetDataA);
+    }
 
     public static Planets getData(String a) throws IOException, InterruptedException {
         var client = HttpClient.newHttpClient();
@@ -30,5 +31,22 @@ public class main {
             planet.add(new Gson().fromJson(arr.get(i).toString(), Planet.class));
         }
         return planets;
+    }
+
+    public static void start(Planets planets) {
+        for (int i = 0; i < planets.getPlanets().size(); i++) {
+            System.out.println(planets.getPlanets().get(i).getPl_name());
+        }
+        String name = sc.nextLine();
+        boolean work = false;
+        for (int i = 0; i < planets.getPlanets().size(); i++) {
+            if (planets.getPlanets().get(i).getPl_name().equalsIgnoreCase(name)) {
+                work = true;
+            }
+        }
+        if (work) {
+            System.out.println("work");
+        }
+
     }
 }
